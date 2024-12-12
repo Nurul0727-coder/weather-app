5a4b09580f6719efeb210cd449ee71b546b875c5
Nurul0727-code <nurul89552017@gmail.com>
2024-12-12 19:30:46 +0800

    test

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,4 @@
 import Image from "next/image";
-// import {GrSearch}from 'react-icon/gr';
 
 export default function Home() {
   return (
@@ -8,10 +7,6 @@ export default function Home() {
         {/* <Card value="day" /> */}
         {/* <SearchInput /> */}
       </div>
-<<<<<<< HEAD
-      <div className="w-[50%] h-[100] bg-slate-400"></div>
-      {/* <div className="w-100 h-200 bg-pink"></div> */}
-=======
       <input className="relative w-[50] h-screen bg-[#0F141E] flex-col-reverse items-center pb-32" />
       {/* <Card value="night" /> */}
       {/*  White circle */}
@@ -19,14 +14,13 @@ export default function Home() {
       {/* top corner */}
       <div className="w-[100px] h-[100px] absolute -left-0 top-[326px] rounded-[36px] bg-[#0F141E] z-10"></div>
       <div className="w-[40px] h-[60px] bg-stone-50 absolute -left-3 top-[380px]"></div>
-      {/* bottom corner */}
+      bottom corner
       <div className="w-[100px] h-[100px] absolute -left-0 top-[574px] rounded-[36px] bg-[#0F141E] z-10"></div>
       <div className="w-[40px] h-[60px] bg-stone-50 absolute -left-3 top-[500px]"></div>
       {/* circles */}
       <div className="w-[600px h-[600px] absolute border rounded-full -left-[300px] top-[200px]"></div>
       <div className="w-[800px h-[800px] absolute border rounded-full -left-[400px] top-[100px]"></div>
->>>>>>> 0c45d7b (background)
+      <div>hi</div>
     </div>
-    
   );
 }
