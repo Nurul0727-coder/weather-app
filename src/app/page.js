70fe4e34472142fecf7c8061574db0cf97fe267e
macbook pro <macbookpro@lovie3.local>
2024-12-12 14:38:59 +0800

    color

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,5 @@
 import Image from "next/image";
+// import {GrSearch}from 'react-icon/gr';
 
 export default function Home() {
   return (
@@ -8,6 +9,8 @@ export default function Home() {
         <input className="p-2 bg-black rounded-2xl mt-6 ml-6" />
       </div>
       <div className="w-[50%] h-[100] bg-slate-400"></div>
+      {/* <div className="w-100 h-200 bg-pink"></div> */}
     </div>
+    
   );
 }
