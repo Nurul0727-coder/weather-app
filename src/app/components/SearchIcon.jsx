9e08e3b40c675ad5bcf24175e7fb6cdcf51e4c87
macbook pro <macbookpro@lovie3.local>
2024-12-14 11:59:04 +0800

    background

--- src/app/components/SearchIcon.jsx
+++ src/app/components/SearchIcon.jsx
@@ -1,3 +1,17 @@
 export const SearchIcon = () => {
-  return <svg xmls="http://www.w3.org/vector.svg"></svg>;
+  return ( 
+  <svg 
+  xmlns="http://www.w3.org/vector.svg"
+    width="36"
+    height="36"
+    viewBox="0 0 48 48"
+    fill="none"
+   >
+    <g opacity="0.2">
+      <path 
+      fill="black"
+      />
+      </g>
+  </svg>
+);
 };
--- src/app/components/SearchInput.jsx
+++ src/app/components/SearchInput.jsx
@@ -1,4 +1,5 @@
-export const SearchInput = () => {
+import { SearchIcon } from "./SearchIcon";
+export const SearchInput = ({ search, setSearch}) => {
   return (
     <div className="border border-transparent drop-shadow-sm rounded-[24px] bg-white w-96 h-120">
       <button>
@@ -8,6 +9,9 @@ export const SearchInput = () => {
         className="w-full ouutline-none mr-1"
         type="text"
         placeholder="City Name..."
+        value={search}
+        onChange={onChangeText}
+        onKeyDown={}
       />
     </div>
   );
